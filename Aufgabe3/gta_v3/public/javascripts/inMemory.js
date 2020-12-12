2d05ff0c6e8fac0ab355afd500ba9d2c2980f78c
Anniway-s <annikawalter01@gmail.com>
2020-12-12 14:00:58 +0100

    Discovery
    Suchfunktion ausgebaut

--- Aufgabe3/gta_v3/public/javascripts/inMemory.js
+++ Aufgabe3/gta_v3/public/javascripts/inMemory.js
@@ -26,14 +26,49 @@ exports.searchGeoTagByTag = function (tag, ArrayGeoTags){
 
     console.log("Kommt an in TagSuche: Tag, : " + tag);
     var ArrayWithTags = new Array();
-    for(var i=0;i<ArrayGeoTags.length;i++){
-        console.log("Kommt an in TagSuche: Tag, ausArrayGeoTags : " + tag +" "+ArrayGeoTags[i].hashtag);
-        if(ArrayGeoTags[i].hashtag==tag){
-            ArrayWithTags.push(ArrayGeoTags[i]);
-            console.log("  *Match!")
+    //Wenn Tag leer ist
+    if(tag == null){
+        for ( var p = 0; p < ArrayGeoTags.length; p++){
+            ArrayWithTags[p] = ArrayGeoTags[p];
+        }
+    //Wenn Tag ein # ist
+    }else if(tag.charAt(0) == "#"){
+        for(var i=0;i<ArrayGeoTags.length;i++){
+            console.log("Kommt an in TagSuche: Tag, ausArrayGeoTags : " + tag +" "+ArrayGeoTags[i].hashtag);
+            if(ArrayGeoTags[i].hashtag==tag){
+                ArrayWithTags.push(ArrayGeoTags[i]);
+                console.log("  *Match!")
+            }
+        }
+    //Wenn Tag ein Name ist
+    }else{
+        //Jedes Objekt von den aktuellen Tags
+        console.log(" Landet in Else (Ist kein Hashtag)")
+        for(var i=0;i<ArrayGeoTags.length;i++){
+            console.log("Kommt an in TagSuche (Name): Tag, ausArrayGeoTags : " + tag +" "+ArrayGeoTags[i].name);
+            if(ArrayGeoTags[i].name.length > tag.length){
+                console.log("Sind nicht gleich lang!")
+                if(ArrayGeoTags[i].name.substring(0, tag.length) ==tag.substring(0, tag.length)){
+                    ArrayWithTags.push(ArrayGeoTags[i]);
+                }else if(ArrayGeoTags[i].hashtag.substring(1, tag.length+1) ==tag.substring(0, tag.length)){
+                    ArrayWithTags.push(ArrayGeoTags[i]);
+                }else{
+                    console.log("Nichts >.<")
+                }
+            }else{
+                console.log("Sind gleich lang!")
+                if(ArrayGeoTags[i].name==tag){
+                    ArrayWithTags.push(ArrayGeoTags[i]);
+                    console.log("  *Match!")
+                }else if(ArrayGeoTags[i].hashtag.substring(1, tag.length+1) ==tag.substring(0, tag.length)){
+                    ArrayWithTags.push(ArrayGeoTags[i]);
+                }else{
+                    console.log("Nichts >.<")
+                }
+            }
         }
     }
-    return ArrayWithTags;
+return ArrayWithTags;
 }
 
 //GeoTag zum Array hinzufügen
@@ -46,4 +81,4 @@ exports.addGeoTag = function (neuerGeoTag, ArrayGeoTags){
 exports.deleteGeoTag = function (name, ArrayGeoTags){
     let pos = ArrayGeoTags.indexOf(name);
     ArrayGeoTags.splice(pos, name);
-}
+};
